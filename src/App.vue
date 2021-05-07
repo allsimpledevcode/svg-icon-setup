8173e6d5f093f30abf4c7a78dc15c82555de3cf1
Lakshmanan <uilaxman@gmail.com>
2021-05-07 15:32:58 +0530

    base setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <SvgIcon
+    name="facebook"
+  />
+  <SvgIcon
+    name="twitter"
+  />
+  <SvgIcon
+    name="tumblr"
+  />
+</template>
+
+<script>
+import SvgIcon from '@/components/SvgIcon'
+
+export default {
+  name: 'App',
+  components: {
+    SvgIcon
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  pluginOptions: {
+    svgSprite: {
+      /*
+       * The directory containing your SVG files.
+       */
+      dir: 'src/assets/icons',
+      /*
+       * The reqex that will be used for the Webpack rule.
+       */
+      test: /\.(svg)(\?.*)?$/,
+      /*
+       * @see https://github.com/kisenka/svg-sprite-loader#configuration
+       */
+      loaderOptions: {
+        extract: true,
+        spriteFilename: 'img/icons.svg' // or 'img/icons.svg' if filenameHashing == false
+      },
+      /*
+       * @see https://github.com/kisenka/svg-sprite-loader#configuration
+       */
+      pluginOptions: {
+        plainSprite: true
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader')
+  }
+}
