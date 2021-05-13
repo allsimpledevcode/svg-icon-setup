45c24ac2326675dcbfbe6fd14cefdd6a732157bd
Lakshmanan <uilaxman@gmail.com>
2021-05-14 02:00:01 +0530

    CSS module used in svg icon component

--- src/components/SvgIcon.vue
+++ src/components/SvgIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg :class="className" xmlns="http://www.w3.org/2000/svg">
+  <svg :class="[$style.svg_icon]" xmlns="http://www.w3.org/2000/svg">
     <title v-if="title">{{ title }}</title>
     <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink"/>
   </svg>
@@ -30,16 +30,12 @@ export default {
 
       return icon.url;
     },
-
-    className() {
-      return 'svg-icon svg-icon--' + this.name;
-    }
   }
 };
 </script>
 
-<style>
-  .svg-icon {
+<style module>
+  .svg_icon {
     fill: currentColor;
     height: 24px;
     width: 24px;
